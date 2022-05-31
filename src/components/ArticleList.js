7569af404f2eaf2b7f710bb5212ee2f7dc3a7ddd
Jocelyn Naarden <tzionone@gmail.com>
2022-05-31 02:59:38 -0400

    Finished Assignment b4 bonus

---
+++ src/components/ArticleList.js
@@ -0,0 +1,20 @@
+// import react from "react"
+import Article from "./Article";
+
+
+function ArticleList({ posts }) {
+  const articleListing = posts.map(post => {
+    // console.log(post)
+    return (
+      <Article key={post.id} title={post.title} date={post.date} preview={post.preview} minutes={post.minutes} />
+    )
+  }
+  )
+  return (
+    <main>
+      {articleListing}
+    </main>
+  )
+}
+
+export default ArticleList
