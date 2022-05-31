3c66bb5f4b54d8841dc74f607f60dbcbde3e84c4
Jocelyn Naarden <tzionone@gmail.com>
2022-05-31 03:38:33 -0400

    Finished Bonus Assignment

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,27 @@
 // import react from "react"
+function minutesToRead(minutes) {
+  let cupsArr = []
+  let bentoArr = []
+  let fullCups = Math.floor(minutes / 5)
+  let fullBento = Math.floor(minutes / 10)
+  if (minutes < 30) {
+    for (let i = 0; i < fullCups; i++) {
+      cupsArr.push('☕')
+    }
+    // console.log("Cups", cupsArr)
+    return cupsArr
+  } else {
+    for (let i = 0; i <= fullBento; i++) {
+      bentoArr.push('🍱')
+      // console.log("Cups", bentoArr)
+    }
+    return bentoArr
+  }
+
+  // get number, see how many times 5 goes into the number
+  // if a full 5, add a coffee cup, if less than five, leave cup alone
+
+}
 
 function Article({
   title,
@@ -6,10 +29,13 @@ function Article({
   preview,
   minutes
 }) {
+
+
   return (
     <article>
       <h3>{title}</h3>
       <small>{date}</small>
+      <p>{minutesToRead(minutes)}</p>
       <p>{preview}</p>
     </article>
   )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,7 +4,7 @@ import Article from "./Article";
 
 function ArticleList({ posts }) {
   const articleListing = posts.map(post => {
-    // console.log(post)
+    console.log("POSTS", post)
     return (
       <Article key={post.id} title={post.title} date={post.date} preview={post.preview} minutes={post.minutes} />
     )
